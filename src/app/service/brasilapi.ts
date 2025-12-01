a8ca1c444eb69439a5a5ec62c1e52df969f08694
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-12-01 16:25:17 -0300

    configurando service para listarUFs

--- src/app/service/brasilapi.ts
+++ src/app/service/brasilapi.ts
@@ -1,12 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Estado } from '../common/brasilapi.models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Brasilapi {
-  
-constructor() {
-  
-}
 
+  baseURL: string = 'https://brasilapi.com.br/api'
+
+  
+constructor(private http: HttpClient) { }
+
+  listarUFs() : Observable<Estado[]> {
+    const path = '/ibge/uf/v1';
+    return this.http.get<Estado[]>(this.baseURL + path);
+  }
 }
